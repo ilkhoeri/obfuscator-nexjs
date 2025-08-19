04cbb5e9f4696758fba92aa3e4d38d84f6130df6
ilkhoeri <khoeriilham@gmail.com>
2025-08-19 22:03:35 +0700

    Move source files to src directory and update configs
    Renamed app, env, and styles directories to src for better project structure. Updated import paths in next.config.js and path aliases in tsconfig.json to reflect new locations. Minor cleanup in next-css-obfuscator.config.ts.

--- next-css-obfuscator.config.ts
+++ next-css-obfuscator.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
-    enable: true,
-    mode: "random", // random | simplify
-    refreshClassConversionJson: false, // recommended set to true if not in production
-    allowExtensions: [".jsx", ".tsx", ".js", ".ts", ".html", ".rsc"],
-    enableMarkers: true,
-  };
+  enable: true,
+  mode: 'random', // random | simplify
+  refreshClassConversionJson: false, // recommended set to true if not in production
+  allowExtensions: ['.jsx', '.tsx', '.js', '.ts', '.html', '.rsc']
+  // enableMarkers: true,
+};
